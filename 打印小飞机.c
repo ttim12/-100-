c49abe5fe5481d0d76fef965cd0b3f1e3282ba06
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-24 15:13:56 +0800

    Add files via upload

---
+++ 打印小飞机.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main () {
+	printf("     **\n     **\n************\n************\n    *  *    \n    *  *    \n");
+	return 0;
+}
+
+
+
+
+
+/*printf("%6s%s\n","*","*");
+	printf("%6s%s\n", "*", "*");
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 12; j++) {
+			printf("%s", "*");
+		}
+		printf("\n");
+	}
+	printf("    %s  %s    \n", "*","*");
+	printf("    %s  %s    \n", "*","*");*/
---
+++ 输出学生信息.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+int main() {
+	printf("Name    Age    Gender\n");
+	for (int i = 0; i < 21; i++) {
+		printf("-");
+	}
+	printf("\nJack    18    man");
+	return 0;
+}
