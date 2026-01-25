b2b02167a7d573fdb9708b667fa8979e1953f590
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-25 12:05:54 +0800

    第二天
    题目：企业发放的奖金根据利润提成。
    
    利润(I)低于或等于10万元时，奖金可提10%；
    利润高于10万元，低于20万元时，低于10万元的部分按10%提成，高于10万元的部分，可提成7.5%；
    20万到40万之间时，高于20万元的部分，可提成5%；
    40万到60万之间时高于40万元的部分，可提成3%；
    60万到100万之间时，高于60万元的部分，可提成1.5%；
    高于100万元时，超过100万元的部分按1%提成。
    从键盘输入当月利润I，求应发放奖金总数？
    
    程序分析：请利用数轴来分界，定位。注意定义时需把奖金定义成双精度浮点(double)型。

---
+++ 奖金求利润.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+int main() {
+int money = 0;
+double sum = 0.0;
+printf("请输入销售额：");
+scanf_s("%d", &money);
+if (money <= 100000) {
+	sum = money * 0.1;
+}
+else if (money <= 200000) {
+	sum = 10000 + (money - 100000) * 0.075;
+}
+else if (money <= 400000) {
+	sum = 17500 + (money - 200000) * 0.05;
+}
+else if (money <= 600000) {
+	sum = 27500 + (money - 400000) * 0.03;
+}
+else if (money <= 1000000) {
+	sum = 33500 + (money - 600000) * 0.015;
+}
+else {
+	sum = 39500 + (money - 1000000) * 0.01;
+}
+printf("奖金总额为：%lf\n", sum);
+	return 0;
+}
+//反过来____________________________________________________________
+//int main() {
+//	float money = 0.0, sum = 0.0;
+//	printf("请输入奖金：");
+//	scanf_s("%f", &money);
+//	if (money < 10000) {
+//		sum = money / 0.1;
+//	}
+//	else if (money < 17500) {
+//		sum = 100000 + (money - 10000) / 0.075;
+//	}
+//	else if (money < 27500) {
+//		sum = 200000 + (money - 17500) / 0.05;
+//	}
+//	else if (money < 33500) {
+//		sum = 400000 + (money - 27500) / 0.03;
+//	}
+//	else if (money < 39500) {
+//		sum = 600000 + (money - 33500) / 0.015;
+//	}
+//	else {
+//		sum = 1000000 + (money - 39500) / 0.01;
+//	}
+//	printf("销售额为：%.0lf\n", sum);
+//
