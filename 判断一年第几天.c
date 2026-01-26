1bf88e7c8ae45dd9e13c41c12d6bfd78c9de984b
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-26 17:43:30 +0800

    第三天
    题目：输入某年某月某日，判断这一天是这一年的第几天？
    
    程序分析：以3月5日为例，应该先把前两个月的加起来，然后再加上5天即本年的第几天，特殊情况，闰年且输入月份大于3时需考虑多加一天。（利用数组好做）

---
+++ 判断一年第几天.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+int main() {
+	int month[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int year = 0, month1 = 0, day = 0, a,sum=0;
+	printf("请输入年 月 日\n");
+	scanf_s("%d%d%d", &year, &month1, &day);
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+		month[1] = 29;
+	}
+	for (a = 1;a<month1;a++) {
+		sum+= month[a - 1];
+	}
+	printf("这是这一年的第%d天\n", sum + day);
+	return 0;
+}
+
+
+
+
+
+
+//_________________________________________________________________________________________________________________________________________
+//错误示例，一开始我不想用数组的方法，想偷懒只用28, 30, 31这3个数字，不过忘了月份的天数没有规律（规律不明显），导致代码错误，逻辑似乎也是比较乱的
+//#include <stdio.h>
+//int main() {
+//	int year, month, day;
+//	int a = 0, b = 0, sum = 0;
+//	printf("输入年月日");
+//	scanf_s("%d%d%d", &year, &month, &day);
+//	if (year % 4 == 0&&year%100!=0) {
+//		sum += 1;
+//	}
+//	for (a = 1; a <=month;) {
+//		sum += 31;
+//		a += 2;		
+//		if (month > 3) {
+//		for (b = 2; b <= month;) {
+//			if (b == 2) { sum += 28; b += 2; break; }
+//			sum += 30;
+//			b += 2;
+//			}
+//		}
+//	}
+//	printf("%d %d", sum, day);
+//		printf("这是这一年的第%d天\n", sum+day);
+//	return 0;
+//}
