f1f3229ec1b954f5a120d6cfe3a6c0125aa3bf40
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-27 15:45:52 +0800

    第四天
    题目：输入三个整数 x、y、z，请把这三个数由小到大输出。
    
    程序分析：我们想办法把最小的数放到 x 上，先将 x 与 y 进行比较，如果 x>y 则将 x 与 y 的值进行交换，然后再用 x 与 z 进行比较，如果 x>z 则将 x 与 z 的值进行交换，这样能使 x 最小

---
+++ 交换数字.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+int main() {
+	int x, y, z;
+	printf("请输入三个数字");
+	scanf_s("%d %d %d", &x, &y, &z);
+	void change(int *a, int *b);
+	if (x > y) {
+		change(&x, &y);
+	}	
+	if (x > z) {
+		change(&x, &z);
+	}
+	if(y>z){
+		change(&y, &z);
+	}
+	printf("排序后：%d %d %d\n", x, y, z);
+	return 0;
+}
+void change(int *a, int *b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
