ae8fa097cc559a140f2517656a3ab0f84d46dfea
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-29 15:10:51 +0800

    第六天
    题目：要求输出国际象棋棋盘。
    
    程序分析：国际象棋棋盘由64个黑白相间的格子组成，分为8行*8列。用i控制行，j来控制列，根据i+j的和的变化来控制输出黑方格，还是白方格。
    
    如果出现乱码情况请参考本博客【C 练习实例7】的解决方法。

---
+++ 国际象棋棋盘.c
@@ -0,0 +1,17 @@
+﻿//利用int的字符编码219为白色的色块来解决
+#include <stdio.h>
+int main() {
+	system("chcp 437");//显示扩展的ASCII码----437 OEM-美国
+	for (int i = 0; i < 8; i++) {
+		for(int j=0;j<4;j++){
+			if (i % 2 == 0) {
+				printf("%c%c  ", 219, 219);
+			}else 
+				printf("  %c%c", 219, 219);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
