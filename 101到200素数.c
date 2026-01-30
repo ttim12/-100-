a1c5f74568c122a89c3cce904d195c3bd1f6e8ba
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-30 12:44:56 +0800

    第六天
    题目：判断 101 到 200 之间的素数。
    
    程序分析：判断素数的方法：用一个数分别去除 2 到 sqrt(这个数)，如果能被整除，则表明此数不是素数，反之是素数。

---
+++ 101到200素数.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main() {
+	int a, b = 1;
+	for (int i = 101; i < 200; i++) {
+		b = 1;
+		for (a = 2; a < (i / 2); a++) {
+			if (i % a == 0) {
+				b = 0;
+				break;
+			}
+		}
+		if (b == 1) { printf("%d\n", i); }
+	}
+
+	return 0;
+}
