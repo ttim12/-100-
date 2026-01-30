4ea8e06e85fea5b758619d3bc57a39eeb189afe0
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-30 13:56:13 +0800

    第六天
    题目：打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数 本身。例如：153是一个"水仙花数"，因为153=1的三次方＋5的三次方＋3的三次方。
    
    程序分析：利用for循环控制100-999个数，每个数分解出个位，十位，百位。

---
+++ 水仙花数.c
@@ -0,0 +1,51 @@
+﻿//简单版计算3位水仙花数
+//#include <stdio.h>
+//int main() {
+//	int number = 100, a, b, c;
+//	for (number = 100; number < 1000; number++) {
+//		a = number / 100;
+//		b = (number / 10) % 10;
+//		c = number % 10;
+//		if(number == a * a * a + b * b * b + c * c * c) {
+//			printf("%d\n", number);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+//-------------------进阶版无限计算水仙花数-----------------------
+#include <stdio.h>
+#include <time.h>
+int main() {
+	long int num, n,sum;
+	clock_t prev_time = clock();
+	for (num = 1;; num++) {
+		sum = 0, n = 0;
+		int tempnum = num;
+		while (tempnum != 0) {
+			tempnum /= 10;
+			n++;//求位数n
+		}
+		int n1 = n, sum0 = num, tempnum1 = num;
+		for (; n >0; n--) {
+			int one = tempnum1 % 10,sumone=1;
+			tempnum1 /= 10;
+			for (int i = 0; i < n1; i++) {
+				sumone*=one;//个位数的n次方
+			}
+			sum += sumone;
+		}
+			if (sum == sum0 && sum0 != 0) {
+				//printf("%d  ", sum0);
+				clock_t now = clock(); 
+				
+				double s = (double)(now - prev_time)/ CLOCKS_PER_SEC; 
+				printf("%d     总耗时%fs    ",sum0,(double)now / CLOCKS_PER_SEC);
+				printf("  (耗时: %lf 秒)\n",  s);
+				prev_time = now; 
+			}
+	}
+	return 0;
+
+}
