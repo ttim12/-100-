67ab15c139d9f75e67a449a82b0605204f2ec15f
程Tim <115853308+ttim12@users.noreply.github.com>
2026-01-30 12:30:00 +0800

    第六天
    题目：古典问题（兔子生崽）：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？（输出前40个月即可）
    
    程序分析：兔子的规律为数列1,1,2,3,5,8,13,21....，即下个月是上两个月之和（从第三个月开始）。

---
+++ 兔子古典.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main() {
+	int month[41] = {0};
+	month[0] = 1, month[1] = 1;
+	for (int i=2;i<41; i++) {
+		month[i] = (month[i - 2] + month[i - 1]);
+	}
+	for (int j=0;j<41; j++) {
+		printf("第%d个月   有%d对    %d只\n", j, month[j],month[j]*2);
+	}
+	return 0;
+}
