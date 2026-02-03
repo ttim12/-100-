ec25a3c5d7e3349f23752d0008fbc9fc0f3ac4a2
程Tim <115853308+ttim12@users.noreply.github.com>
2026-02-03 14:22:17 +0800

    第七天
    题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
    
    程序分析：(a>b)?a:b这是条件运算符的基本例子。

---
+++ 进阶条件嵌套判断.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main() {
+	int score;char result;
+	printf("请输入成绩：");
+	scanf_s("%d", &score);
+	result = (score >= 90) ? 65 : (score >= 60 ? 66 : 67);
+	printf("%c", result);
+
+
+	return 0;
+}
