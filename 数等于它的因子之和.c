20538890737a0bce41631a5268fb124395f7a94d
程Tim <115853308+ttim12@users.noreply.github.com>
2026-02-04 14:58:39 +0800

    Refactor 完数 program for better output formatting

--- 数等于它的因子之和.c
+++ 数等于它的因子之和.c
@@ -1,24 +1,22 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 int main()  {
-	int a = 0, b = 0,sum=0;
+	int a = 0,A, b = 0,sum=0;
 	for(sum=1;sum<1000;sum++){
 		b = 0;
 		for (a = 1; a < sum; a++) {
 			if (sum % a == 0) {
 				b += a;
+				A = a;
 			}
 		}
 		if(b==sum){
-			printf("%d is a 完数  %d=",sum,sum);
+			printf("%d is a 完数  %d=1",sum,sum);
 			b = 0;
-			for (a = 1; a < sum; a++) {
+			for (a = 2; a < sum; a++) {
 				if (sum % a == 0) {
-					printf("%d",a);
+					printf("+%d",a);
 					b += a;
-					if (b < sum) {
-						printf("+");
-					}
-					if(b==sum){
+					if(A==a){
 						printf("\n");
 					}
 				}
@@ -27,3 +25,36 @@ int main()  {
 	}
 	return 0;
 }
+
+
+
+// ﻿#include <stdio.h>
+// int main()  {
+// 	int a = 0, b = 0,sum=0;
+// 	for(sum=1;sum<1000;sum++){
+// 		b = 0;
+// 		for (a = 1; a < sum; a++) {
+// 			if (sum % a == 0) {
+// 				b += a;
+// 			}
+// 		}
+// 		if(b==sum){
+// 			printf("%d is a 完数  %d=",sum,sum);
+// 			b = 0;
+// 			for (a = 1; a < sum; a++) {
+// 				if (sum % a == 0) {
+// 					printf("%d",a);
+// 					b += a;
+// 					if (b < sum) {
+// 						printf("+");
+// 					}
+// 					if(b==sum){
+// 						printf("\n");
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return 0;
+
+// }
