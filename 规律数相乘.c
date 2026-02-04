f597106f9ae838e895cdecbc093c6039a53f523b
程Tim <115853308+ttim12@users.noreply.github.com>
2026-02-04 13:56:29 +0800

    第九天
    题目：求 s=a+aa+aaa+aaaa+aa...a 的值，其中 a 是一个数字，例如 2+22+222+2222+22222 (此时共有5个数相加)，几个数相加有键盘控制。
    
    程序分析：关键是计算出每一项的值。

---
+++ 规律数相乘.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main() {
+	int a, n,sum=0;
+	scanf_s("%d%d", &a, &n);
+	for(;n>0;n--) {
+		sum += a;
+		a = ((a * 10) + (a%10));
+		printf("%d\n", a);	
+	}
+	printf("%d\n", sum);
+	return 0;
+}
