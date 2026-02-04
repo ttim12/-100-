11720e31d5599d7990c91657c27e4574d405a70c
程Tim <115853308+ttim12@users.noreply.github.com>
2026-02-04 14:53:37 +0800

    第九天
    题目：一个数如果恰好等于它的因子之和，这个数就称为"完数"，例如 6=1＋2＋3 ，请编程找出 1000 以内的所有完数。
    
    程序分析：请参照：C 练习实例14。

---
+++ 数等于它的因子之和.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+int main()  {
+	int a = 0, b = 0,sum=0;
+	for(sum=1;sum<1000;sum++){
+		b = 0;
+		for (a = 1; a < sum; a++) {
+			if (sum % a == 0) {
+				b += a;
+			}
+		}
+		if(b==sum){
+			printf("%d is a 完数  %d=",sum,sum);
+			b = 0;
+			for (a = 1; a < sum; a++) {
+				if (sum % a == 0) {
+					printf("%d",a);
+					b += a;
+					if (b < sum) {
+						printf("+");
+					}
+					if(b==sum){
+						printf("\n");
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
